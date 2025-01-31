7fda9b3098ba7450f77468fce4caccb40b60649c
Ajit  Thapa <athapa30@myseneca.ca>
2025-01-31 11:50:40 -0500

    lab done

---
+++ lab3a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Author ID: [athapa30] 
+
+def return_text_value():
+    return "Good Morning Terry"
+
+def return_number_value():
+    value1 = 10
+    value2 = 5 
+    return value1 + value2
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
